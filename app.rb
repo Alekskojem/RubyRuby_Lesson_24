a3ac2a66ddf6730c54df6e897905c1947bf6189a
alekskojem <alekskojem@gmail.com>
2017-08-21 22:50:56 +0300

    Add hash validation

--- app.rb
+++ app.rb
@@ -24,22 +24,20 @@ post '/visit' do
 	@barber = params[:barber]
 	@color = params[:color]
 
-	if @username == ''
-		@error = 'ВВЕДИТЕ ИМЯ!'
-		
-		end
-	if @phone == ''
-		@error = 'ВВЕДИТЕ НОМЕР ТЕЛЕФОНА'
-
-	end
-
-	if @datetime == ''
-		@error = 'Укажите точную дату и время!'
-	end
-	
-	if error !=''
+	hh = { :username =>'ВВЕДИТЕ ИМЯ!', 
+		   :phone => 'ВВЕДИТЕ НОМЕР ТЕЛЕФОНА',
+		   :datetime => 'Укажите точную дату и время!' }
+	#для каждой пары ключ-значение
+	hh.each do |kye, value|
+	if params [key] == ''
+		#переменной error присвоить value из хеша hh
+		#(a value из хеша hh это ссобщение об ошибке)
+		# переменной error присвоить сообщение об ошибке
+		@error == hh[key]
+		#вернуть предстивление visit
 		return erb :visit
 	end
+end
 
 	
 	erb "OK, Имя посетителя #{@username}, телфон #{@phone}, указано время #{@datetime}, мастера #{@barber}, #{@color}."
