9b4d98bd4630d98c5d9e088dda84b6e9118f42b9
alekskojem <alekskojem@gmail.com>
2017-08-21 20:37:37 +0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/About' do
+	erb :About
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+	
+	erb "OK, Имя посетителя #{@username}, телфон #{@phone}, указано время #{@datetime}, мастера #{@barber}, #{@color}."
+end
